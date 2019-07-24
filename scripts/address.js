fbf63d625c73f46cfb46c2ba6a13dfe79cb1c70f
Carl Barringer <cbarringer@Carls-MacBook-Pro.local>
2019-07-24 13:25:20 -0500

    added functionality to clear inputs

--- scripts/address.js
+++ scripts/address.js
@@ -3,8 +3,10 @@
 // todo get data
 // todo HTML representation
 // todo Render to DOM
+// todo I can purchase {thing} at {location}
 const addressContainer = document.querySelector("#addressList")
 const favContainer = document.querySelector("#favThings")
+
 const htmlRep = (name, address) => {
     return `
     <div>
@@ -13,12 +15,12 @@ const htmlRep = (name, address) => {
     </div>
     `
 }
-
 const favHTMLRep = (favoriteThing, whereToPurchase) => {
     return `
     <div>
     <h3>${favoriteThing}</h3>
     <p>${whereToPurchase}</p>
+    <p>I can purchase ${favoriteThing} at ${whereToPurchase}.</p>
     </div>
     `
 }
@@ -28,20 +30,28 @@ const favHTMLRep = (favoriteThing, whereToPurchase) => {
 document.querySelector("#saveEntry").addEventListener("click", ()=>{
     const name = document.querySelector("#fullName").value
     const address = document.querySelector("#address").value
-    console.log("hello")
     console.log(name)
-
     const personalAddress = htmlRep(name, address)
     console.log(personalAddress)
     addressContainer.innerHTML += personalAddress
+    clearForm()
+    
 })
 
 document.querySelector("#saveToWishList").addEventListener("click", () => {
     const favoriteThing = document.querySelector("#favoriteItem").value
     const whereToPurchase = document.querySelector("#purchaseLocation").value
     console.log(favoriteThing)
+    const favCollection  = favHTMLRep(favoriteThing, whereToPurchase)
+    console.log(favCollection)
+    favContainer.innerHTML += favCollection
 })
 
+function clearForm () {
+    document.querySelector("#fullName").value = ""
+    document.querySelector("#address").value = ""
+    console.log("name in clearForm():", document.querySelector("#fullName").value)
+}
 
 
 
